504854c5403f93ff595bc46bda354d23c99cc7c8
lbugg <logangbugg@gmail.com>
2024-02-05 11:53:11 -0500

    first commit

--- screens/Login.js
+++ screens/Login.js
@@ -53,14 +53,30 @@ const Login = ({loggedInState, loggedInStates,setLoggedInState})=>{
 
              {/* <Text style={{fontSize:20, marginVertical:30, textAlign:'center', color:'#818181'}}>Or</Text> */}
 
-            <TouchableOpacity
-               style={styles.sendButton}
+             <TouchableOpacity 
+              style={styles.sendButton}
               onPress={async ()=>{
-                console.log('Hello Logan')
+              console.log(phoneNumber+' Button was pressed')
+  
+              const sendTextResponse=await fetch(
+              'https://dev.stedi.me/twofactorlogin/'+phoneNumber,
+              {
+              method:'POST',
+              headers:{
+              'content-type':'application/text'
+              }
+              }
+              )
+              const sendTextResponseData = await sendTextResponse.text();
+              if(sendTextResponse.status!=200){//invalid phone number, send them to the signup page
+              await Alert.alert("Did you type your number correctly? "+phoneNumber);
+              } else{
+              setLoggedInState(loggedInStates.LOGGING_IN);
+              }
               }}
-            >
-              <Text style={{color:'white'}}>Send</Text>      
-            </TouchableOpacity>
+              >
+              <Text style={{color:'white'}}>Send</Text>
+              </TouchableOpacity>
     
           </View>
         )
